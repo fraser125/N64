d10c2697212f87f203c89c1664e88c64390da59c
fraser <jkwall@hotmail.com>
2018-12-24 22:22:55 -0800

    Updated tests to match documentation

--- UnitTests/TestSource/MIPS_I___TestTheTest.asm
+++ UnitTests/TestSource/MIPS_I___TestTheTest.asm
@@ -10,30 +10,30 @@ base $1FC00000
 Start:
 	
 //# Test the Test Instructions
-	syscallUTE(0, 1, 'F')	// Output Expected!
-	syscallUTE(0, 2, 'P')	// Output Expected!
+	syscallUTE(1, 0, 1, 'F')	// Output Expected!
+	syscallUTE(1, 0, 2, 'P')	// Output Expected!
 	
 //# Test beq True, nop ignored for now
 	beq r0, r0, good3
 	nop
 bad3:
-	syscallUTE(0, 3, 'F')
+	syscallUTE(1, 0, 3, 'F')
 good3:
-	syscallUTE(0, 3, 'P') 
+	syscallUTE(1, 0, 3, 'P') 
 
 
 //# Test beq True Negative
 	b test4
 	nop
 good4:
-	syscallUTE(0, 4, 'P')
+	syscallUTE(1, 0, 4, 'P')
 	b continue5
 	nop
 test4:
 	beq r0, r0, good4
 	nop
 bad4:
-	syscallUTE(0, 4, 'F') 
+	syscallUTE(1, 0, 4, 'F') 
 continue5:	
 	
 	
@@ -45,13 +45,13 @@ continue5:
 	beq r0, r0, good6
 	nop
 bad6:
-	syscallUTE(0, 5, 'F') 
-	syscallUTE(0, 6, 'F') 
+	syscallUTE(1, 0, 5, 'F') 
+	syscallUTE(1, 0, 6, 'F') 
 	b continue7
 	nop
 good6:
-	syscallUTE(0, 5, 'P')
-	syscallUTE(0, 6, 'P')
+	syscallUTE(1, 0, 5, 'P')
+	syscallUTE(1, 0, 6, 'P')
 continue7:
 
 //# Test addi instruction
@@ -60,11 +60,11 @@ continue7:
 	beq t0, t1, good7
 	nop
 bad7:
-	syscallUTE(0, 7, 'F')
+	syscallUTE(1, 0, 7, 'F')
 	b continue8
 	nop
 good7:
-	syscallUTE(0, 7, 'P')
+	syscallUTE(1, 0, 7, 'P')
 continue8:
 	
 //# Test addi negative numbers
@@ -73,11 +73,11 @@ continue8:
 	beq t0, t1, good8
 	nop
 bad8:
-	syscallUTE(0, 8, 'F')
+	syscallUTE(1, 0, 8, 'F')
 	b continue9
 	nop
 good8:
-	syscallUTE(0, 8, 'P')
+	syscallUTE(1, 0, 8, 'P')
 continue9:
 	
 //# Test addi negative numbers
@@ -89,11 +89,11 @@ continue9:
 	beq t0, t1, good9
 	nop
 bad9:
-	syscallUTE(0, 9, 'F')
+	syscallUTE(1, 0, 9, 'F')
 	b continue10
 	nop
 good9:
-	syscallUTE(0, 9, 'P')
+	syscallUTE(1, 0, 9, 'P')
 continue10:
 	
 //# Test addiu instruction
@@ -102,11 +102,11 @@ continue10:
 	beq t0, t1, good10
 	nop
 bad10:
-	syscallUTE(0, 10, 'F')
+	syscallUTE(1, 0, 10, 'F')
 	b continue11
 	nop
 good10:
-	syscallUTE(0, 10, 'P')
+	syscallUTE(1, 0, 10, 'P')
 continue11:
 	
 //# Test addi negative numbers
@@ -115,10 +115,10 @@ continue11:
 	beq t0, t1, good11
 	nop
 bad11:
-	syscallUTE(0, 11, 'F')
+	syscallUTE(1, 0, 11, 'F')
 	beq r0, r0, continue10
 good11:
-	syscallUTE(0, 11, 'P')
+	syscallUTE(1, 0, 11, 'P')
 continue12:
 	
 //# Test addi negative numbers
@@ -130,12 +130,14 @@ continue12:
 	beq t0, t1, good12
 	nop
 bad12:
-	syscallUTE(0, 12, 'F')
+	syscallUTE(1, 0, 12, 'F')
 	b continue13
 	nop
 good12:
-	syscallUTE(0, 12, 'P')
+	syscallUTE(1, 0, 12, 'P')
 continue13:
 TheEnd:
 
 fixed_gap(Start, 0x0700)
+origin external_test_steps
+instructionCount(TheEnd) // Macro only supports 1024 * 1024 = 1,048,575
